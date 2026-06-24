9897c7afe004b1fe07eed13eab6bf062c94984e5
osamait04-sketch <osamait04@gmail.com>
2026-06-24 16:18:01 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// إخبار السيرفر بقراءة الملفات الثابتة من المجلد الرئيسي
+// إخبار السيرفر بقراءة الملفات الثابتة من المجلد الرئيسي للمشروع
 app.use(express.static(path.join(__dirname)));
 
-// عند فتح الموقع، يقوم السيرفر بإرسال ملف index.html مباشرة
+// عند فتح الرابط، يقوم السيرفر بقراءة وعرض ملف index.html مباشرة
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// تشغيل السيرفر على البورت المحدد لـ Render
 app.listen(PORT, () => {
     console.log(`Pharmacy Server is running on port ${PORT}`);
 });
